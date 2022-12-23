3bc9285d5d40e916461d6fd19787eec10d590c9a
Nazar Mosula <nmosula@gmail.com>
2022-12-23 18:46:02 +0200

    Create color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const btnStart = document.querySelector("button[data-start]");
+const btnStop = document.querySelector("button[data-stop]");
+let timerId = null;
 
+btnStart.addEventListener("click", onClickStart);
+ btnStop.addEventListener("click", onClickStop);
+
+function onClickStart() {
+
+    btnStart.setAttribute("disabled", "");
+    timerId = setInterval (() => {
+        let newColor = getRandomHexColor();
+        document.body.style.background = newColor;
+    }, 1000);
+}
+
+function onClickStop() {
+    clearInterval(timerId);
+    btnStart.removeAttribute("disabled");
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
